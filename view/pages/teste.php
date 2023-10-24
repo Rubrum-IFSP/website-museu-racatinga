a474e2e3385f553f5daf55f9d381858f4725dcf4
Felipe Fernandes Alves <112494130+PatoDeSapatos@users.noreply.github.com>
2023-10-24 11:39:46 -0300

    login wrapper test started

---
+++ view/pages/teste.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/loginWrapper.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="amigoMuseu-wrapper">
+        <form action="#" class="account-wrapper login">
+            <div class="userinfo-box">
+                <label for="nome">Nome:</label>
+                <input type="text">
+            </div>
+            
+            <div class="userinfo-box">
+                <label for="">Senha: </label>
+                <input type="text">
+            </div>
+
+            <button>Login</button>
+        </form>
+
+        <form action="#" class="account-wrapper register">
+            <div class="userinfo-area">
+                <div class="userinfo-box">
+                    <label for="nome">Nome:</label>
+                    <input type="text">
+                </div>
+                
+                <div class="userinfo-box">
+                    <label for="">Senha: </label>
+                    <input type="text">
+                </div>
+            </div>
+
+            <div class="userinfo-area">
+                <div class="userinfo-box">
+                    <label for="">CPF: </label>
+                    <input type="text">
+                </div>
+
+                <div class="userinfo-box">
+                    <label for="">RG: </label>
+                    <input type="text">
+                </div>
+            </div>
+
+            <button>Registrar</button>
+        </form>
+    </div>
+</body>
+</html>
