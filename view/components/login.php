8359caadb9229d72d6bf5ba63a9842751e43ebd0
Felipe Fernandes Alves <felipefernanalves@gmail.com>
2023-10-31 13:32:04 -0300

    login css done

---
+++ view/components/login.php
@@ -0,0 +1,59 @@
+
+
+<div class="amigoMuseu-wrapper">
+<span class="material-symbols-outlined">close</span>
+
+<form action="#" class="account-wrapper login">
+        <h1>Bem Vindo de Volta!</h1>
+        <div class="userinfo-box">
+            <label for="nome">Nome:</label>
+            <input type="text">
+        </div>
+        
+        <div class="userinfo-box">
+            <label for="">Senha: </label>
+            <input type="text">
+        </div>
+
+        <button>Entrar</button>
+
+        <div class="change-wrapper">
+            <p>Não tem uma conta? <span class="register-button">Registre-se agora!</span></p>
+        </div>
+    </form>
+
+    <form action="#" class="account-wrapper register">
+        <h1>Bem Vindo ao Museu!</h1>
+        <div class="userinfo-area">
+            <div class="userinfo-box">
+                <label for="nome">Nome:</label>
+                <input type="text">
+            </div>
+            
+            <div class="userinfo-box">
+                <label for="">Senha: </label>
+                <input type="text">
+            </div>
+        </div>
+
+        <div class="userinfo-area">
+            <div class="userinfo-box">
+                <label for="">CPF: </label>
+                <input type="text">
+            </div>
+
+            <div class="userinfo-box">
+                <label for="">RG: </label>
+                <input type="text">
+            </div>
+        </div>
+
+        <button>Registrar</button>
+
+        <div class="change-wrapper">
+            <p>Já tem uma conta? <span class="login-button">Entre agora!</span></p>
+        </div>
+    </form>
+</div>
+
+<script src="../js/amAccountWrapper.js"></script>
