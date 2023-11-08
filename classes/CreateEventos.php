6893d348b9ea2e3e094702d87cd2f547715c8bf8
Pombo8 <117785799+Pombo8@users.noreply.github.com>
2023-11-08 14:42:24 -0300

    Add files via upload

--- classes/CreateEventos.php
+++ classes/CreateEventos.php
@@ -5,7 +5,12 @@
             $mysqli = $this->conectar();
 
             $query = "INSERT INTO `Evento`(`nome`, `dataEvento`, `descricao`) VALUES ('$nome',$data,'$desc')";
+            $queryVerificar = mysqli_query($mysqli, "SELECT * FROM Evento where nome='$nome'");
+            if(mysqli_query($mysqli, $queryVerificar));
+            {
+
             $queryCreate = mysqli_query($mysqli, $query);
+        }
         }
 
         public function update($nome, $data, $desc){
@@ -25,7 +30,7 @@
         }
         public function delete(String $evento){
             $mysqli =$this->conectar();
-            $query = "SELECT id FROM Evento where nome = '$nome'";
+            $query = "SELECT id FROM Evento where nome = '$evento'";
             $resultQuery = mysqli_query($mysqli, $query);
 
             if(mysqli_num_rows($resultQuery)>0){
@@ -34,12 +39,29 @@
                     break;
                 }
             }
-            $query = "DELETE FROM Pecas, Evento where idEvento = Evento.id";
-            $result = mysqli_query($mysqli, $query);
+            $query = "DELETE FROM Pecas WHERE idEvento=$selectedProduct";
+            mysqli_query($mysqli, $query);
+            $query ="DELETE FROM Evento WHERE id=$selectedProduct";
+            mysqli_query($mysqli, $query);
+
         }
         
+        public function mostrar(){
+            $query = "SELECT nome, descricao, dataEvento FROM Evento";
+            $listar = mysqli_query($this->conectar(),$query);
+
+            while($linha=mysqli_fetch_array($listar)){
+                echo "<div class='container-event'>";
+                    echo "<p><spam class='nome-event'>".$linha[0]."</spam></p>";
+                    echo "<p><spam class='desc-event'>".$linha[1]."</spam></p>";
+                    echo "<p><spam class='data-event'>".$linha[2]."</spam></p>";
+                echo "</div>";
+            }
+        }
+
     }
 
+        
     
         
 ?>
