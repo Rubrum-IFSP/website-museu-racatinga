66e9e86fe4d66afbbe842c809b92a26732dafadd
Pombo8 <117785799+Pombo8@users.noreply.github.com>
2023-11-08 19:46:38 -0300

    Add files via upload
    agr createIngressos mostra e compra ingressos

--- classes/CreateIngressos.php
+++ classes/CreateIngressos.php
@@ -34,7 +34,43 @@
                 header("location: ../view/pages/ingressosPagina.php");
             }
         }
+
+        //recebe $_session['username']
+        public function mostrarIngressos($username){
+            $query = "SELECT id FROM Pessoa where nome = '$username'";
+            $resultQuery = mysqli_query($this->conectar(), $query);
+
+            if(mysqli_num_rows($resultQuery)>0){
+                while($row = mysqli_fetch_assoc($resultQuery)){
+                    $selectedProduct = $row['id'];
+                    break;
+                }
+            }
+            $queryIngressos = "SELECT IngressoEvento.codigo, IngressoEvento.dataCompra FROM IngressoEvento where IngressoEvento.idPessoa = $selectedProduct";
+            $resultIngressos = mysqli_query($this->conectar(), $queryIngressos);
+
+            $resultIdEvento= mysqli_query($this->conectar(), "SELECT idEvento from IngressoEvento, Pessoa WHERE $selectedProduct = IngressoEvento.idPessoa");
+            if(mysqli_num_rows($resultIdEvento)>0){
+                while ($row = mysqli_fetch_assoc($resultIdEvento)){
+                    $idEvento = $row['idEvento'];
+                    break;  
+                }
+            }
+            $resultNomeEvento= mysqli_query($this->conectar(),"SELECT nome from Evento, IngressoEvento WHERE  $idEvento = Evento.id ");
+
+
+
+            while($linha=mysqli_fetch_array($resultIngressos)){
+                echo "<div class='container-ingresso'>";
+                    echo "<p><spam class='content-ingresso'>Codigo: </spam>".$linha[0]. "</p>";
+                    echo "<p class='content-ingresso'>Data de Compra: ".$linha[1]."</p>";
+                    while($linhaNomeEvento=mysqli_fetch_array($resultNomeEvento)){
+                        echo "<p class='content-ingresso'>Evento: ".$linhaNomeEvento[0]."</p>";
+                    }
+                echo "</div>";
+        }
         
 
     }
+}
     ?>
