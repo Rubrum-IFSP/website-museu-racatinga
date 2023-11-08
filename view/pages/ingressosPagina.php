a0debab6af1e0d58e6c1c34905ec12ed4a598b66
Pombo8 <117785799+Pombo8@users.noreply.github.com>
2023-11-08 19:12:03 -0300

    Add files via upload

--- view/pages/ingressosPagina.php
+++ view/pages/ingressosPagina.php
@@ -1,2 +1,9 @@
-falta o css e html das seguintes paginas :
-- createADM , deleteADM, updateAdm, loginAdmPagina, mudarSenhaAdm,cadastro, login e mudarSenhaAmigo,
+<?php
+    require "../../classes/Conexao.php";
+    require "../../classes/createIngressos.php";
+    session_start();
+    // aq é a compra do ingresso, vc vai ter um form,***************** APENAS AMIGOS DO MUSEU PODEM ENTRAR NA PÁGINA INGRESSOS****************************
+    // 1 input = <select> com o nome dos eventos * <option value='evento1' name='$id'>
+    // 2 input check box "CERTEZA QUE QUER COMPRAR?"
+    // 3 input  BUTTON COMPRAR INGRESSO
+?>
