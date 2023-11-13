364418e9c09cb67623b92518080410a7a6d9ac69
Felipe Fernandes Alves <felipefernanalves@gmail.com>
2023-11-13 09:54:59 -0300

    new event image css

--- classes/model/evento/EventoManager.php
+++ classes/model/evento/EventoManager.php
@@ -75,21 +75,24 @@
 
             while($linha=mysqli_fetch_array($listar)){
                 echo "<div class='container-event'>";
-                    echo "<div class='information-upper'>";
-                        echo "<figure>";
-                            echo "<img src='../images/imagem.png'>";
-                        echo "</figure>"; 
-                        echo "<p class='evento-title'>$linha[0]</p>";
-                        echo "<p class='evento-title'>$linha[2]</p>";
-                    echo "</div>";
-                    echo "<div class='information-down'>";
-                        echo "<p class='evento-title'>Descrição: </p>";
-                        echo "<p>$linha[1]</p>";
+                    echo "<figure>";
+                        echo "<img src='../images/imagem.png'>";
+                        echo "<div class='evento-background'></div>";
+                    echo "</figure>"; 
+                    echo "<div>";
+                        echo "<div class='information-upper'>";
+                            echo "<p class='evento-title'>$linha[0]</p>";
+                            echo "<p class='evento-title'>$linha[2]</p>";
+                        echo "</div>";
+                        echo "<div class='information-down'>";
+                            echo "<p class='evento-title'>Descrição: </p>";
+                            echo "<p>$linha[1]</p>";
+                        echo "</div>";
                     echo "</div>";
                 echo "</div>";
             }
         }
-        public function getEvento($nomeEvento) : EventoVO | bool {
+        public function getEvento($nomeEvento) : EventoVO {
             $mysqli = $this->conectar();
             $query = "SELECT * FROM Evento where nome = '$nomeEvento'";
             $resultQuery = mysqli_query($mysqli, $query);
